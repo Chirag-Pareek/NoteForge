ca3d70bec14e7e8309ff1fad5d796b2af269a346
Chirag-Pareek <chiragpareek677@gmail.com>
2026-01-31 21:59:48 +0530

    31

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 import 'app_text_styles.dart';
-import 'app_radius.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
