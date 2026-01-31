1e0803e41b35d4f80939fc547658822613cd71a4
Chirag-Pareek <chiragpareek677@gmail.com>
2026-01-31 21:59:39 +0530

    31

---
+++ lib/core/routes/route_generator.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../../features/auth/presentation/welcome_screen.dart';
+import '../../features/auth/presentation/login_screen.dart';
+import '../../features/auth/presentation/signup_screen.dart';
+import '../../features/auth/presentation/profile_setup_screen.dart';
+import '../navigation/main_shell.dart';
+import 'app_routes.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.welcome:
+        return MaterialPageRoute(builder: (_) => const WelcomeScreen());
+      
+      case AppRoutes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      
+      case AppRoutes.signup:
+        return MaterialPageRoute(builder: (_) => const SignupScreen());
+      
+      case AppRoutes.profileSetup:
+        return MaterialPageRoute(builder: (_) => const ProfileSetupScreen());
+      
+      case AppRoutes.home:
+        return MaterialPageRoute(builder: (_) => const MainShell());
+      
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
