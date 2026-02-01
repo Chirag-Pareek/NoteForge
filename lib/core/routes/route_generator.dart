47af4c4358395ed27cb1548739ce20f02886d7ff
Chirag-Pareek <chiragpareek677@gmail.com>
2026-02-01 18:35:55 +0530

    new

--- lib/core/routes/route_generator.dart
+++ lib/core/routes/route_generator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteforge/features/home/presentation/edit_profile_screen.dart';
 import '../../features/auth/presentation/welcome_screen.dart';
 import '../../features/auth/presentation/login_screen.dart';
 import '../../features/auth/presentation/signup_screen.dart';
@@ -11,25 +12,26 @@ class RouteGenerator {
     switch (settings.name) {
       case AppRoutes.welcome:
         return MaterialPageRoute(builder: (_) => const WelcomeScreen());
-      
+
       case AppRoutes.login:
         return MaterialPageRoute(builder: (_) => const LoginScreen());
-      
+
       case AppRoutes.signup:
         return MaterialPageRoute(builder: (_) => const SignupScreen());
-      
+
       case AppRoutes.profileSetup:
         return MaterialPageRoute(builder: (_) => const ProfileSetupScreen());
-      
+
       case AppRoutes.home:
         return MaterialPageRoute(builder: (_) => const MainShell());
-      
+
+      case AppRoutes.profilEdit:
+        return MaterialPageRoute(builder: (_) => const EditProfileScreen());
+
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
-            body: Center(
-              child: Text('No route defined for ${settings.name}'),
-            ),
+            body: Center(child: Text('No route defined for ${settings.name}')),
           ),
         );
     }
